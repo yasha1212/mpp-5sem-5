99817f5e7270aee55e43f21913938f96a13bee80
yasha1212 <doenglish1@mail.ru>
2020-11-15 23:58:38 +0300

    Add: dependency provider logic for transient implementations

--- DependencyInjectionContainer/DependencyProvider.cs
+++ DependencyInjectionContainer/DependencyProvider.cs
@@ -1,10 +1,80 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Dynamic;
+using System.Reflection;
 using System.Text;
 
 namespace DependencyInjectionContainer
 {
     public class DependencyProvider
     {
+        private readonly Dictionary<Type, List<ImplementationInfo>> dependencies;
+
+        public DependencyProvider(DependenciesConfiguration configuration)
+        {
+            dependencies = configuration.Dependencies;
+        }
+
+        public T Resolve<T>() where T: class
+        {
+            Type dependencyType = typeof(T);
+
+            return (T)Resolve(dependencyType);
+        }
+
+        private object Resolve(Type dependencyType)
+        {
+            if (dependencyType.IsGenericType && (dependencyType.GetGenericTypeDefinition() == typeof(IEnumerable<>)))
+            {
+                return GetAllImplementations(dependencyType.GetGenericArguments()[0]);
+            }
+
+            if (!dependencies.ContainsKey(dependencyType))
+            {
+                return null;
+            }
+
+            var implementationInfo = dependencies[dependencyType][0];
+
+            if (!dependencyType.IsAssignableFrom(implementationInfo.ImplementationType))
+            {
+                return null;
+            }
+
+            if (implementationInfo.Lifetime == Lifetime.Transient)
+            {
+                return CreateInstance(implementationInfo.ImplementationType);
+            }
+
+            return null;
+        }
+
+        private IEnumerable GetAllImplementations(Type dependencyType)
+        {
+            List<ImplementationInfo> implementations = dependencies[dependencyType];
+            Type collectionType = typeof(List<>).MakeGenericType(dependencyType);
+            IList instances = (IList)Activator.CreateInstance(collectionType);
+
+            foreach (var implementation in implementations)
+            {
+                instances.Add(CreateInstance(implementation.ImplementationType));
+            }
+
+            return instances;
+        }
+
+        private object CreateInstance(Type type)
+        {
+            ConstructorInfo constructor = type.GetConstructors()[0];
+            var parameters = new List<object>();
+
+            foreach (var parameter in constructor.GetParameters())
+            {
+                parameters.Add(Resolve(parameter.ParameterType));
+            }
+
+            return Activator.CreateInstance(type, parameters.ToArray());
+        }
     }
 }
