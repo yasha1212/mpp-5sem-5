661b984abbf4e84bc102cf2deee1a46d295cd038
yasha1212 <doenglish1@mail.ru>
2020-11-16 00:12:56 +0300

    Add: singleton instances container class

---
+++ DependencyInjectionContainer/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DependencyInjectionContainer
+{
+    public class Singleton
+    {
+        private static Dictionary<Type, object> instances = new Dictionary<Type, object>();
+        private static object syncLocker = new object();
+
+        public delegate object InstanceCreator(Type type);
+
+        public static object GetInstance(Type type, InstanceCreator createInstance)
+        {
+            if (!instances.ContainsKey(type))
+            {
+                lock (syncLocker)
+                {
+                    if (!instances.ContainsKey(type))
+                    {
+                        instances.Add(type, createInstance(type));
+                    }
+                }
+            }
+
+            return instances[type];
+        } 
+    }
+}
