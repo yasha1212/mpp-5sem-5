880da62f7cc4e60dc3b7d0fde489b2c97cb55e18
yasha1212 <doenglish1@mail.ru>
2020-11-15 22:58:12 +0300

    Add: dependencies configuration and initial solution file

---
+++ DependencyInjectionContainer/DependenciesConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DependencyInjectionContainer
+{
+    public class DependenciesConfiguration
+    {
+        public Dictionary<Type, List<ImplementationInfo>> Dependencies { get; }
+
+        public DependenciesConfiguration()
+        {
+            Dependencies = new Dictionary<Type, List<ImplementationInfo>>();
+        }
+
+        public void Register<TDependency, TImplementation>(Lifetime lifetime = Lifetime.Transient) where TDependency: class where TImplementation: TDependency
+        {
+            var implementationInfo = new ImplementationInfo(typeof(TImplementation), lifetime);
+
+            if (Dependencies.ContainsKey(typeof(TDependency)))
+            {
+                Dependencies[typeof(TDependency)].Add(implementationInfo);
+            }
+            else
+            {
+                Dependencies.Add(typeof(TDependency), new List<ImplementationInfo>());
+                Dependencies[typeof(TDependency)].Add(implementationInfo);
+            }
+        }
+    }
+}
---
+++ DependencyInjectionContainer/ImplementationInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DependencyInjectionContainer
+{
+    public enum Lifetime
+    {
+        Singleton,
+        Transient
+    }
+
+    public class ImplementationInfo
+    {
+        public Lifetime Lifetime { get; }
+        public Type ImplementationType { get; }
+        
+        public ImplementationInfo(Type type, Lifetime lifetime)
+        {
+            Lifetime = lifetime;
+            ImplementationType = type;
+        }
+    }
+}
